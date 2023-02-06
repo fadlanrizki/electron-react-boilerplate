134c9a3483028a2a2d1e9b396dddc687ce61167d
fadlan rizki <fadlanrizky19@gmail.com>
2023-02-07 05:28:05 +0700

    add button component

---
+++ src/component/common/button/buttonSave/ButtonSave.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './../module/ButtonSave.module.css';
+
+const ButtonSave = () => {
+  return (
+    <div>
+      <button>Save</button>
+    </div>
+  );
+};
+
+export default ButtonSave;
